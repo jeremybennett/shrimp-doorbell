26bcb014dd73bf796ed34a12d97559d45208f22a
Jeremy Bennett <jeremy.bennett@embecosm.com>
2013-12-29 18:30:05 +0000

    Low power version.
    	* arduino-code/doorbell/doorbell.ino: include avr/sleep.h and
    	avr/wdt.h.
    	(melody): Set as Westminster chimes and increase to 16 entries
    	(noteDurations): Set 16 entries.
    	(setup): 16 notes in loop. Set code to disable ADC and power down
    	on finish.

--- arduino-code/doorbell/doorbell.ino
+++ arduino-code/doorbell/doorbell.ino
@@ -16,32 +16,47 @@ This example code is in the public domain.
  
  */
  #include "pitches.h"
+ #include <avr/sleep.h>
+ #include <avr/wdt.h>
 
 // notes in the melody:
 int melody[] = {
-  NOTE_C4, NOTE_G3,NOTE_G3, NOTE_A3, NOTE_G3,0, NOTE_B3, NOTE_C4};
+  NOTE_E4,  NOTE_GS4, NOTE_FS4, NOTE_B3,
+  NOTE_E4,  NOTE_FS4, NOTE_GS4, NOTE_E4,
+  NOTE_GS4, NOTE_E4,  NOTE_FS4, NOTE_B3,
+  NOTE_B3,  NOTE_FS4, NOTE_GS4, NOTE_E4,
+  };
 
 // note durations: 4 = quarter note, 8 = eighth note, etc.:
 int noteDurations[] = {
-  4, 8, 8, 4,4,4,4,4 };
+  2, 2, 2, 2,
+  2, 2, 2, 2,
+  2, 2, 2, 2,
+  2, 2, 2, 2 };
 
 void setup() {
   // iterate over the notes of the melody:
-  for (int thisNote = 0; thisNote < 8; thisNote++) {
+  for (int thisNote = 0; thisNote < 16; thisNote++) {
 
     // to calculate the note duration, take one second 
     // divided by the note type.
     //e.g. quarter note = 1000 / 4, eighth note = 1000/8, etc.
     int noteDuration = 1000/noteDurations[thisNote];
-    tone(8, melody[thisNote],noteDuration);
+    tone (8, melody[thisNote],noteDuration);
 
     // to distinguish the notes, set a minimum time between them.
     // the note's duration + 30% seems to work well:
     int pauseBetweenNotes = noteDuration * 1.30;
-    delay(pauseBetweenNotes);
+    delay (pauseBetweenNotes);
     // stop the tone playing:
-    noTone(8);
+    noTone (8);
   }
+  
+  // Now go to sleep until a reset
+  set_sleep_mode (SLEEP_MODE_PWR_DOWN);
+  ADCSRA |= (0<<ADEN);                     // disable ADC
+  sleep_enable ();
+  sleep_mode();
 }
 
 void loop() {
